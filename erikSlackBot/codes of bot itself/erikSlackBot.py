26b553b064ab0b3beb75e521060c95d4d8f1abf7
Erik <erik@bigbek.com>
2017-05-02 02:25:54 +0400

    New modules added

--- erikSlackBot/codes of bot itself/erikSlackBot.py
+++ erikSlackBot/codes of bot itself/erikSlackBot.py
@@ -3,13 +3,16 @@ import time
 from slackclient import SlackClient
 from random import randint
 from chatterbot import ChatBot
+import urllib
+from mtranslate import translate
+import wikipedia
 
 # starterbot's ID as an environment variable
 BOT_ID = os.environ.get("BOT_ID")
 HIGHSCORE = 0;
 # constants
-AT_BOT = "<@" + BOT_ID + ">:"
-EXAMPLE_COMMAND = "jarvis_ask"
+AT_BOT = "<@" + BOT_ID + ">"
+EXAMPLE_COMMAND = "vlad"
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
@@ -31,6 +34,95 @@ chatbot = ChatBot(
 
 # Train based on the english corpus
 chatbot.train("chatterbot.corpus.english")
+
+
+
+
+
+
+lang = ["af:", "sq:", "ar:", "az:", "eu:", "bn:", "be:", "bg:", "ca:", "zh-cn:", "zh-tw:",\
+        "hr:", "cs:", "da:", "nl:", "en:", "eo:", "et:", "tl:", "fi:", "fr:", "gl:", "ka:",\
+        "de:", "el:", "gu:", "ht:", "iw:", "hi:", "hu:", "is:", "id:", "ga:", "it:", "ja:",\
+        "kn:", "ko:", "la:", "lv:", "lt:", "mk:", "ms:", "mt:", "no:", "fa:", "pl:", "pt:",\
+        "ro:", "ru:", "sr:", "sk:", "sl:", "es:", "sw:", "sv:", "ta:", "te:", "th:", "tr:",\
+        "uk:", "ur:", "vi:", "cy:", "yi:"]
+
+wiki_liblang = ["af:", "sq:", "ar:", "az:", "eu:", "bn:", "be:", "bg:", "ca:", "zh:",\
+                "hr:", "cs:", "da:", "nl:", "en:", "eo:", "et:", "tl:", "fi:", "fr:", "gl:", "ka:",\
+                "de:", "el:", "gu:", "ht:", "iw:", "hi:", "hu:", "is:", "id:", "ga:", "it:", "ja:",\
+                "kn:", "ko:", "la:", "lv:", "lt:", "mk:", "ms:", "mt:", "no:", "fa:", "pl:", "pt:",\
+                "ro:", "ru:", "sr:", "sk:", "sl:", "es:", "sw:", "sv:", "ta:", "te:", "th:", "tr:",\
+                "uk:", "ur:", "vi:", "cy:", "yi:"]
+
+wiki_lang = ""
+wiki_line = 1
+
+
+def check_valid_lang(command):
+    command = (command[10:len(command):] + ":").strip()
+    for elem in wiki_liblang:
+        if (elem == command):
+            return True
+    else:
+        return False
+
+def handle_translation(command):
+    for elem in lang:
+        if (command.startswith(elem)):
+            to_translate = command[len(elem):len(command):].strip()
+            origin = "auto"
+            for elem2 in lang:
+                if (to_translate.startswith(elem2)):
+                    if (elem2 != "zh-cn:" and elem2 != "zh-tw:"):
+                        origin = to_translate[0:2]
+                    else:
+                        origin = to_translate[0:5]
+                    to_translate = to_translate[len(elem2):len(to_translate):]
+            else:
+                if (to_translate.startswith("auto:")):
+                    to_translate = to_translate[len("auto:"):len(to_translate):]
+            if (to_translate != "" and elem != "zh-cn:" and elem != "zh-tw:"):
+                response = translate(to_translate.encode('utf-8'), elem[0:2:], origin)
+            elif(to_translate != ""):
+                if (elem == "zh-cn:"):
+                    elem = "zh-CN:"
+                else:
+                    elem = "zh-TW:"
+                response = translate(to_translate.encode('utf-8'), elem[0:5:], origin)
+            else:
+                response = "Nothing to translate"
+            return response
+    else:
+        return ""
+
+def find_definition(command):
+    if(command.startswith("what is a ")):
+        command = command[10:len(command)]
+    elif (command.startswith("what is ") or command.startswith("who are ")):
+        command = command[8:len(command)]
+    elif (command.startswith("what are ") or command.startswith("what's a ")):
+        command = command[9:len(command)]
+    elif (command.startswith("who is ")):
+        command = command[7:len(command)]
+    elif (command.startswith("what's")):
+        command = command[6:len(command)]
+    if (command[len(command) - 1] == '?'):
+        command = command[0:len(command) - 1]
+    try:
+        if (wiki_lang != ""):
+            wikipedia.set_lang(wiki_lang[:-1])
+            if (wiki_lang != "zh:"):
+                lang = wiki_lang
+            else:
+                lang = "zh-cn:"
+            command = command.strip()
+            command = handle_translation(lang + command).lower()
+        command = command.title()
+        response = '```' + wikipedia.summary(command, sentences=wiki_line) + '```'
+    except:
+        response = "Need a clear one"
+    return response
+
 
 
 def randomBasketball():
@@ -72,12 +164,80 @@ def handle_command(command, channel):
         """
     response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
         "* command with numbers, delimited by spaces."
+
+
+
+
+def handle_command(command, channel):
+    response = ""
+    command = command.strip()
     
+    """global declaration"""
+    
+    global wiki_lang
+    global wiki_line
+    
+    if command.startswith("wiki line:"):
+        check_line = int(command[10:len(command):])
+        if (check_line > 0 and check_line <= 100):
+            wiki_line = int(command[10:len(command):].strip())
+            response = "current wiki line is: *%d*" % wiki_line
+        else:
+            response = "`limit size 100`"
+    if command.startswith("wiki lang:"):
+        if (check_valid_lang(command)):
+            wiki_lang = (command[10:len(command):] + ":").strip()
+            response = "current wiki language is: *%s*" % wiki_lang[0:-1:]
+        else:
+            response = "`Invalid language`"
+    if (handle_translation(command) != ""):
+        response = "> *" + handle_translation(command) + "*"
+    if (response != ""):
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+        return
+    
+    """ here we translate the command to eng"""
+    command = handle_translation(("en:" + command)).lower()
+    char_replace = u"\u2019"
+    command = command.replace(char_replace, "'")
+    
+    """check if it's a question """
+    if command.startswith(("what's ", "what's a", "what is ",\
+                           "what is a ", "what are ", "who is ", "who are ")):
+        response = find_definition(command)
+    elif command.startswith("wiki stat"):
+        if (wiki_lang == ""):
+            response = "current wiki lang is en\ncurrent wiki line is " + str(wiki_line)
+        else:
+            response = "current wiki lang is " + wiki_lang[:-1] + "\ncurrent wiki line is " + str(wiki_line)
+    else:
+            #response = "I don't understand"
+            print("LoopMaker")
+    if (response != ""):
+            slack_client.api_call("chat.postMessage", channel=channel,
+                    text=response, as_user=True)
+
+
+
+
     if command.startswith(EXAMPLE_COMMAND):
         if command.endswith("time") or command.endswith("Time"):
             timer()
         elif command.endswith("play_basketball") or command.endswith("random") or command.endswith("random_game"):
+            
             randomBasketball()
+
+        elif any(substring in command for substring in ["lawyer", "law", "legal", "airport"]):
+            
+            response = "Please refer to this group. (https://www.airportlawyer.org/)"
+            slack_client.api_call("chat.postMessage", channel=channel,
+                      text=response, as_user=True)
+        elif any(substring in command for substring in ["visa", "H1" , "H-1" , "immigrate" , "Transfer"]):
+            
+            response = "I found a place where you can find help. (https://visabot.co/)"
+            slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+
         else:
             response = "Master i know not what you speak of!"
             # Get a response to an input statement
@@ -86,7 +246,8 @@ def handle_command(command, channel):
     else:
         #response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
         "* command with numbers or words, delimited by spaces or _."
-        response =str(chatbot.get_response(command))
+        print(command)
+        response = str(chatbot.get_response(command))
         slack_client.api_call("chat.postMessage", channel=channel,
                       text=response, as_user=True)
 
--- erikSlackBot/codes of bot itself/print_bot_id.py
+++ erikSlackBot/codes of bot itself/print_bot_id.py
@@ -2,7 +2,7 @@ import os
 from slackclient import SlackClient
 
 
-BOT_NAME = 'homeworkbot'
+BOT_NAME = 'nosferatu'
 
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
